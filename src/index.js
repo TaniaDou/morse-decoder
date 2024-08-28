224ab329c6b0ed46239a54e6344b75a362b959eb
Tania Dou <t.doump@gmail.com>
2024-08-28 16:19:03 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let symbols = {
+        '00': '',
+        '10': '.',
+        '11': '-',
+    }
+
+    let sentence = [];
+
+    for(i = 0; i < expr.length; i += 10 ) {
+       let section = expr.slice(i, i + 10);
+       if (dec = '**********') {
+        dec = ' ';
+       }    
+    
+        let letter = [];
+
+        for(i= 0; i < 10; i += 2) {
+        let symbol = section.slice(i, i + 2);
+        letter.push(symbol);
+    }
+    sentence.push(letter);
 }
+    let codedSentence = [];
+    for(let item of sentence){
+        let codedLetter = '';
+        item.forEach(el => el in symbols ? codedLetter += symbols[el] : codedLetter += '');
+        codedSentence.push(codedLetter);
+    }
+    let result = '';
+    codedSentence.forEach(el => result += MORSE_TABLE[el]);
+    return result;
+}
+
 
 module.exports = {
     decode
